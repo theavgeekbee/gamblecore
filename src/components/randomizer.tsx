7a9d2a1748ddc2bac5f987628c4b7d7272daff30
Nathan Lee <me@nwlee.tech>
2025-03-01 17:07:07 -0600

    feat: update randomizer

--- src/components/randomizer.tsx
+++ src/components/randomizer.tsx
@@ -11,12 +11,8 @@ export default function Randomizer() {
             setProgress(0);
 
             const buy_or_sell = Math.random() < 0.5 ? "BUY" : "SELL";
-            const mkt_or_stop = Math.random() < 0.8 ? "MKT" : "STOP";
-            let price = Math.random() * 100;
 
-            price = Math.trunc(price * 100) / 100;
-
-            setTrade(`${buy_or_sell} ${mkt_or_stop} ${price}`)
+            setTrade(`${buy_or_sell} MKT`)
         }, 5000);
 
         const progressInterval = setInterval(() => {
