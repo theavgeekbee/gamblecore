8d643fd07e1e035f6419731cb08d001fb7fb08d8
Nathan Lee <me@nwlee.tech>
2025-03-02 03:57:29 -0600

    feat: branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import {SigmaTerminal} from "@/components/sigmaTerminal";
-import Randomizer from "@/components/randomizer";
+import Image from "next/image";
 import {Suspense} from "react";
 import TradingPanel from "@/components/tradingpanel";
 import InventoryList from "@/components/inventorylist";
 import ItemShop from "@/components/itemshop";
+import logo from "@/../public/logo.png";
 
 export default function Home() {
     return (
         <main>
             <div className={"container"}>
                 <div className={"title"}>
-                    <h1>SigmaTerminal</h1>
+
+                    <h1><Image src={logo} alt={"Logo"} height={50} width={50} /> SigmaTerminal</h1>
                     <h2>Rizz First / Then Griddy</h2>
                 </div>
                 <div className={"row"}>
